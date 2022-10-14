a86a5bce5ce576a571c195a9cd9e9fe073c962b4
VishalNehete <vishalnehete05@gmail.com>
2022-10-14 11:32:30 +0530

    LeetCode Day1 & Day2

---
+++ src/LeetCodeProblems/Arrays/MaxPerimeterOfTriangle.java
@@ -0,0 +1,26 @@
+package LeetCodeProblems.Arrays;
+
+import java.util.Arrays;
+
+public class MaxPerimeterOfTriangle {
+    public static void main(String[] args) {
+        int arr[]={2,1,1,6,8};
+
+        System.out.println("Maximum perimeter triangle that can be formed is: "+getMaxPerimeter(arr));
+    }
+
+    private static int getMaxPerimeter(int[] arr) {
+
+        //Sort the array to get the max elements at last of the array
+        Arrays.sort(arr);
+
+        //Iterate from last till 3rd element of array with condition (a+b)>c
+        // If satisfied return sum of those 3 elements
+        for(int i= arr.length-3;i>0;i--){
+            if((arr[i]+arr[i+1]) > arr[i+2]){
+                return arr[i]+arr[i+1] + arr[i+2];
+            }
+        }
+        return 0;
+    }
+}
---
+++ src/LeetCodeProblems/LinkedList/CheckForLoop.java
@@ -0,0 +1,94 @@
+package LeetCodeProblems.LinkedList;
+
+public class CheckForLoop {
+
+    class Node{
+        Node next;
+        int value;
+
+        public Node(int value) {
+           this.value=value;
+        }
+    }
+    static Node head=null;
+    static Node tail=null;
+
+    public void insertNode(int value){
+        //Create a node with the value to e inserted
+        Node newNode=new Node(value);
+
+        //check if the list is empty
+        if(head==null && tail == null) {
+            head = newNode;
+            tail = newNode;
+        }
+
+        //if not empty
+        tail.next=newNode;
+        newNode.next=null;
+        tail=newNode;
+
+    }
+
+    public void createALoop(int pos){
+        //if invalid position provided dont create a loop
+        if(pos == -1)
+            return;
+
+
+        Node current=head;
+        int count=0;
+        //Reach till the position where you want to point from tail to create a loop
+        while(current!=null && pos!=count){
+            current=current.next;
+            count++;
+        }
+        tail.next=current;
+    }
+
+    public void printList(){
+        Node current =head;
+        while(current!=null){
+            System.out.println(current.value);
+            current=current.next;
+        }
+    }
+
+    public boolean hasLoop(){
+        //two pointers to check is list has loop
+        Node slowPointer = head;
+        Node fastPointer = head.next;
+
+        //If these two pointers meet at any position in list then the list contains a loop.
+        //Slow pointer gets incremented by 1 Node and fast pointer by 2 nodes of list.
+        while(fastPointer !=null && fastPointer.next != null){
+            if(fastPointer == slowPointer)
+                return true;
+
+            slowPointer=slowPointer.next;
+            fastPointer=fastPointer.next.next;
+        }
+
+
+        return false;
+    }
+
+
+
+    public static void main(String[] args) {
+        CheckForLoop ck = new CheckForLoop();
+
+        ck.insertNode(20);
+        ck.insertNode(21);
+        ck.insertNode(15);
+        ck.insertNode(25);
+        ck.createALoop(2);
+//        ck.printList();
+
+        System.out.println("Above List hasLoop: "+ck.hasLoop());
+
+
+    }
+
+
+}
